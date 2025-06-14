118bf14ddfd46dc2bfb6a8f76e2b56591038ef8b
Bhuvnesh Jangid <bhuvnesh@example.com>
2025-06-14 11:00:26 +0530

    add form in ExpenseForm component

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const ExpenseForm = () => {
+
+    const[title,setTitle]=useState()
+    const[amount,setAmount]=useState()
+    const[type,setType]=useState("expense")
+
+
+   
+
     return(
-        <h1>form</h1>
+        <div>
+            <form style={{ display:"flex",flexDirection:"column"}}  onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="enter title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <input
+                type="number"
+                placeholder="enter amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                />
+
+                <select value={type} onChange={(e) => setType(e.target.value)}>
+                    <option value="expense">Expense</option>
+                    <option value="income">Income</option>
+                </select>
+
+                <button type="submit">Add</button>
+
+            </form>
+
+          
+        </div>
     )
 }
 
